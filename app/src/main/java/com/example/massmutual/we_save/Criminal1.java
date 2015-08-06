7d793822aaf7dd81078225dcbcf0a44b9d92072c
TatyanaMedina <taty2.medina@gmail.com>
2015-08-06 12:50:20 -0400

    This is the pages part

---
+++ app/src/main/java/com/example/massmutual/we_save/Criminal1.java
@@ -0,0 +1,14 @@
+package com.example.massmutual.we_save;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by massmutual on 8/6/2015.
+ */
+public class Criminal1 extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/massmutual/we_save/Criminal2.java
@@ -0,0 +1,15 @@
+package com.example.massmutual.we_save;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by massmutual on 8/6/2015.
+ */
+public class Criminal2 extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criminal2);
+    }
+}
